685d885c2e43c6b1b860bd9656a6337d427e151b
Alexander Almaleh <sashoalm@gmail.com>
2017-04-18 22:14:44 +0300

    Fresh.

---
+++ HelloWebApi/Default.aspx.designer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace HelloWebApi
+{
+	
+	public partial class Default
+	{
+		protected System.Web.UI.WebControls.Button button1;
+	}
+}
+
